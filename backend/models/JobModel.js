af76c2b0ad0e5359fe62f6fcb327dca8d830eb73
dendonpaul <dendonpaul@gmail.com>
2023-03-18 19:54:54 +0530

    JobModel created

--- backend/models/JobModel.js
+++ backend/models/JobModel.js
@@ -1,3 +1,11 @@
 const mongoose = require("mongoose");
 
-const JobSchema = new mongoose.Schema({});
+const JobSchema = new mongoose.Schema({
+  name: String,
+  jtitle: String,
+  technologies: Array,
+});
+
+const JobModel = mongoose.model("jobs", JobSchema);
+
+module.exports = JobSchema;
