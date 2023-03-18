b76fabe1bbe1f201357a9556018cef20aee7f939
dendonpaul <dendonpaul@gmail.com>
2023-03-18 19:56:58 +0530

    evn port  issuefixed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5001;
 require("dotenv").config();
+const port = process.env.PORT || 5001;
+
 const Connection = require("./db/Connection");
 
 if (Connection) {
