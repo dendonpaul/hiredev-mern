0e0e8420f8a3a182a06b4d77d9774663857513ef
dendonpaul <dendonpaul@gmail.com>
2023-03-19 10:51:13 +0530

    Routes updated

--- backend/routes/HireDevRoutes.js
+++ backend/routes/HireDevRoutes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const DevController = require("../controllers/DevController");
 const CompController = require("../controllers/CompController");
+const JobController = require("../controllers/JobController");
 
 router.post("/register-dev", DevController.register);
 router.post("/register-comp", CompController.register);
+router.post("/add-job", JobController.register);
 
 module.exports = router;
